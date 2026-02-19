e6a5d416f77a6c6b6c1b6b73b0f3d51c1be899f2
Johnrey Bacal <johnrey.bacal@yahoo.com>
2026-02-19 18:00:14 +0800

    tutorial level

--- scripts/map.gd
+++ scripts/map.gd
@@ -4,11 +4,12 @@ class_name Map
 enum MapCell {
     Ground, Wall, Trap
 }
+# Ground = DELETE
+# Only trap will be drawn for now
 
 @onready var coordinates_label: Label = $CoordinatesLabel
-@onready var ground_hint: Node2D = $Hint/Ground
-@onready var wall_hint: Node2D = $Hint/Wall
-@onready var trap_hint: Node2D = $Hint/Trap
+@onready var left_mouse_hint: Node2D = $Hint/LeftMouse
+@onready var right_mouse_hint: Node2D = $Hint/RightMouse
 
 var current_cell: MapCell = MapCell.Ground
 
@@ -18,7 +19,9 @@ var cell_coords = {
     MapCell.Trap: Vector2i(1, 1),
 }
 
-var is_mouse_down = false
+# var is_mouse_down = false
+var is_mouse_left = false
+var is_mouse_right = false
 
 var min_y = 2
 var max_y = 21
@@ -29,7 +32,7 @@ var max_x = 40
 
 func _ready() -> void:
     MapManager.map_ready.connect(_setup_map)
-    _reset_hint_position()
+    # _reset_hint_position()
 
 func _setup_map():
     var cells = MapManager.player_map
@@ -56,25 +59,35 @@ func _setup_map():
         tile_map_layer.set_cell(Vector2i(i, 24), 0, Vector2(-1, -1))
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("use_ground"):
-        current_cell = MapCell.Ground
-        _reset_hint_position()
-    if event.is_action_pressed("use_wall"):
-        current_cell = MapCell.Wall
-        _reset_hint_position()
-    if event.is_action_pressed("use_trap"):
-        current_cell = MapCell.Trap
-        _reset_hint_position()
-
+    # if event.is_action_pressed("use_ground"):
+    #     current_cell = MapCell.Ground
+    #     _reset_hint_position()
+    # if event.is_action_pressed("use_wall"):
+    #     current_cell = MapCell.Wall
+    #     _reset_hint_position()
+    # if event.is_action_pressed("use_trap"):
+    #     current_cell = MapCell.Trap
+    #     _reset_hint_position()
     # https://www.youtube.com/watch?v=U_TGOgp5-pc
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT:
-            is_mouse_down = event.pressed
-            if is_mouse_down:
+            is_mouse_left = event.pressed
+            if is_mouse_left:
+                left_mouse_hint.skew = deg_to_rad(randf_range(-5, 5))
                 _draw_cell()
+            else:
+                left_mouse_hint.skew = 0
+
+        elif event.button_index == MOUSE_BUTTON_RIGHT:
+            is_mouse_right = event.pressed
+            if is_mouse_right:
+                right_mouse_hint.skew = deg_to_rad(randf_range(-5, 5))
+                _draw_cell()
+            else:
+                right_mouse_hint.skew = 0
 
     if event is InputEventMouseMotion:
-        if is_mouse_down:
+        if is_mouse_left or is_mouse_right:
             _draw_cell()
         var coords := tile_map_layer.local_to_map(get_local_mouse_position())
         var x = coords.x # - 1
@@ -84,23 +97,48 @@ func _input(event: InputEvent) -> void:
             coordinates_label.visible = true
         else:
             coordinates_label.visible = false
+            
+    if is_mouse_left:
+        left_mouse_hint.skew = deg_to_rad(randf_range(-5, 5))
+        left_mouse_hint.scale = Vector2(randf_range(0.95, 1.05), randf_range(0.95, 1.05))
+    else:
+        left_mouse_hint.skew = 0
+        left_mouse_hint.scale = Vector2.ONE
 
-func _reset_hint_position():
-    ground_hint.position.y = -2 if current_cell == MapCell.Ground else 0
-    wall_hint.position.y = -2 if current_cell == MapCell.Wall else 0
-    trap_hint.position.y = -2 if current_cell == MapCell.Trap else 0
+    if is_mouse_right:
+        right_mouse_hint.skew = deg_to_rad(randf_range(-5, 5))
+        right_mouse_hint.scale = Vector2(randf_range(0.95, 1.05), randf_range(0.95, 1.05))
+    else:
+        right_mouse_hint.skew = 0
+        right_mouse_hint.scale = Vector2.ONE
+
+# func _reset_hint_position():
+#     ground_hint.position.y = -2 if current_cell == MapCell.Ground else 0
+#     wall_hint.position.y = -2 if current_cell == MapCell.Wall else 0
+#     trap_hint.position.y = -2 if current_cell == MapCell.Trap else 0
 
 
 func _draw_cell():
+    if is_mouse_left:
+        current_cell = MapCell.Trap
+    elif is_mouse_right:
+        current_cell = MapCell.Ground
+    else:
+        return
+
     var coords := tile_map_layer.local_to_map(get_local_mouse_position())
     var x = coords.x
     var y = coords.y
 
-    var cell_data = MapManager.map[y][x]
+    var cell_data = MapManager.player_map_initial[y][x]
     if cell_data in [1, 2]:
         return
     # don't draw outside
     if x < min_x or x > max_x or y < min_y or y > max_y:
         return
+    
     tile_map_layer.set_cell(coords, 0, cell_coords[current_cell])
     MapManager.update_player_map(Vector2i(x, y), current_cell)
+
+func draw_cell_on_coords(coords, value: int):
+    tile_map_layer.set_cell(coords, 0, cell_coords[value])
---
+++ scripts/tutorial.gd
@@ -0,0 +1,170 @@
+extends Node2D
+
+@onready var instruction: Node2D = $Instruction
+@onready var wasd: Node2D = $Instruction/Sprites/WASD
+@onready var space_sprite: Sprite2D = $Instruction/Sprites/SpaceSprite
+@onready var trap_sprite: Sprite2D = $Instruction/Sprites/TrapSprite
+@onready var q_sprite: Sprite2D = $Instruction/Sprites/SpriteQ
+@onready var instruction_label: Label = $Instruction/Label
+
+@onready var tile_map_layer: TileMapLayer = $TileMapLayer
+
+var w_duration: float = 0
+var a_duration: float = 0
+var s_duration: float = 0
+var d_duration: float = 0
+
+var dash_number: int = 0
+
+var stage = 0
+# 0 wasd
+# 1 dash
+# 2 watch out for trap
+# 3 enemies
+
+var camera: Camera2D
+var player: Player
+var map: Map
+
+var input_threshold = .5
+
+func _ready() -> void:
+    player = get_tree().get_first_node_in_group("Player")
+    map = get_tree().get_first_node_in_group("Map")
+    camera = get_tree().get_first_node_in_group("Camera")
+    
+    player.hp = 99999
+
+    camera.limit_bottom = 180 + 16
+
+    wasd.visible = false
+    space_sprite.visible = false
+    trap_sprite.visible = false
+    q_sprite.visible = false
+
+    MapManager.map_updated.connect(_on_map_update)
+    MapManager.player_map_initial[6][20] = 2
+    MapManager.player_map[6][20] = 2
+    MapManager.update_player_map(Vector2i(20, 6), 2)
+    map.draw_cell_on_coords(Vector2i(20, 6), 2)
+
+    _update_instructions()
+
+func _physics_process(delta: float) -> void:
+    # WASD
+    if stage == 0:
+        if Input.is_action_pressed("move_up"):
+            w_duration += delta
+            _shake_object($Instruction/Sprites/WASD/SpriteW)
+        if Input.is_action_pressed("move_left"):
+            a_duration += delta
+            _shake_object($Instruction/Sprites/WASD/SpriteA)
+        if Input.is_action_pressed("move_down"):
+            s_duration += delta
+            _shake_object($Instruction/Sprites/WASD/SpriteS)
+        if Input.is_action_pressed("move_right"):
+            d_duration += delta
+            _shake_object($Instruction/Sprites/WASD/SpriteD)
+
+        if w_duration > input_threshold and a_duration > input_threshold and s_duration > input_threshold and d_duration > input_threshold:
+            stage = 1
+            _update_instructions()
+    # DASH
+    elif stage == 1:
+        if Input.is_action_just_pressed("dash") and player.dash_cooldown <= 0:
+            dash_number += 1
+
+        if player.dash_duration > 0:
+            _shake_object(space_sprite)
+
+        if dash_number > 2:
+            stage = 2
+            _update_instructions()
+    # Trigger trap
+    elif stage == 2:
+        if BulletTimeManager.bullet_timer > 0:
+            stage = 3
+            _update_instructions()
+    # Mark trap
+    elif stage == 3:
+        if Input.is_action_pressed("toggle_map"):
+            _shake_object(q_sprite)
+    if stage > 1 and stage < 5:
+        instruction.position.x = max(camera.position.x - 100, camera.limit_left + 100)
+    elif stage == 5:
+        instruction.position.x = max(camera.position.x - 100, camera.limit_left + 100)
+        instruction.position.y = max(camera.position.y, camera.limit_top + 100)
+
+func _update_instructions():
+    # WASD
+    if stage == 0:
+        wasd.visible = true
+        instruction_label.text = "Use WASD to move around."
+    # DASH
+    elif stage == 1:
+        wasd.visible = false
+        space_sprite.visible = true
+        instruction_label.text = "Use SPACE to dash."
+    # Trigger trap
+    elif stage == 2:
+        _remove_first_area()
+        space_sprite.visible = false
+        trap_sprite.visible = true
+        instruction_label.text = "Think fast. Dungeons are full of traps."
+    # Mark trap
+    elif stage == 3:
+        trap_sprite.visible = false
+        q_sprite.visible = true
+        instruction_label.text = "Your Job: Mark traps in the map."
+    # Proceed to monsters
+    elif stage == 4:
+        q_sprite.visible = false
+        instruction_label.text = "You may proceed. Traps are reset after a while."
+    # Monsters
+    elif stage == 5:
+        instruction_label.text = "Monsters will kill you. Stand still to hide or lead them to traps."
+    # Deal with Monsters
+    elif stage == 6:
+        instruction.visible = false
+    elif stage == 7:
+        instruction.visible = true
+        instruction.position = Vector2(536, 231)
+        instruction_label.text = "That's it! Mark traps and survive."
+
+
+func _on_map_update(coords: Vector2i, value: int):
+    if coords == Vector2i(19, 6) and value == 2 and stage == 3:
+        stage = 4
+        var cell = Vector2(-1, -1)
+        for i in range(5, 8):
+            tile_map_layer.set_cell(Vector2i(21, i), 0, cell)
+        _update_instructions()
+        
+
+func _remove_first_area():
+    var cell = Vector2(-1, -1)
+    for i in range(3, 11):
+        tile_map_layer.set_cell(Vector2i(11, i), 0, cell)
+
+
+func _on_third_area_entrance_area_body_entered(body: Node2D) -> void:
+    if body is Player and stage == 4:
+        stage = 5
+        camera.limit_left = 16
+        camera.limit_bottom = 360 + 16
+        _update_instructions()
+        
+func _on_third_area_exit_area_body_entered(body: Node2D) -> void:
+    if body is Player and stage == 5:
+        stage = 6
+        _update_instructions()
+
+func _on_fourth_area_entrance_area_body_entered(body: Node2D) -> void:
+    if body is Player and stage == 6:
+        stage = 7
+        _update_instructions()
+
+
+func _shake_object(node: Node2D):
+    node.skew = deg_to_rad(randf_range(-5, 5))
+    node.scale = Vector2(randf_range(0.95, 1.1), randf_range(0.95, 1.1))
